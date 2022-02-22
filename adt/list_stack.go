230af340f77f8cd6648f86401e70d8e9ba255b61
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-02-22 17:38:49 +0300

    ADT: Refactoring

--- adt/list_stack.go
+++ adt/list_stack.go
@@ -1,35 +1,16 @@
 package adt
 
-type Node struct {
-	value      interface{}
-	prev, next *Node
-}
-
 type ListStack struct {
 	item *Node
 }
 
 func (s *ListStack) Push(value interface{}) {
-	if s.item == nil {
-		s.item = new(Node)
-		s.item.value = value
-	} else {
-		newItem := new(Node)
-		newItem.value = value
-		newItem.prev = s.item
-		s.item.next = newItem
-		s.item = newItem
-	}
+	s.item = &Node{value: value, prev: s.item}
 }
 
 func (s *ListStack) Pop() interface{} {
 	result := s.item.value
-	if s.item.prev != nil {
-		s.item = s.item.prev
-		s.item.next = nil
-	} else {
-		s.item = nil
-	}
+	s.item = s.item.prev
 	return result
 }
 
