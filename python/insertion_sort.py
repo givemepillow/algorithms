2c19b38c700bbd11ae4bb925de682ca147a3c52b
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-05-19 00:41:39 +0300

    New algorithm.

---
+++ python/insertion_sort.py
@@ -0,0 +1,13 @@
+def sort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j and array[j - 1] > array[j]:
+            temp = array[j]
+            array[j] = array[j - 1]
+            array[j - 1] = temp
+            j -= 1
+    return array
+
+
+some_array = [10, 1, 2, 0, 2, 3, 4, 6, 4, 2, 3, 4545, 6, 7, 43, -1, -17]
+print(*sort(some_array))
